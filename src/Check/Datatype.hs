f8dcc5cf4f0abbabf9aced4dc4ce1773efb53314
Isaac Elliott <isaace71295@gmail.com>
2020-05-28 18:53:55 +1000

    building axiom type. should it have any TMetas in it?

--- src/Check/Datatype.hs
+++ src/Check/Datatype.hs
@@ -14,6 +14,8 @@ import Control.Monad.State (MonadState)
 import Data.Foldable (foldlM, traverse_)
 import Data.Map (Map)
 import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
 import Data.Text (Text)
 import Data.Vector (Vector)
 import qualified Data.Vector as Vector
@@ -28,7 +30,7 @@ import Check.Entailment
   )
 import Check.Kind (checkKind, unifyKind)
 import Check.TypeError (TypeError)
-import IR (Constraint(..), KMeta, Kind(..), substKMeta)
+import IR (Constraint(..), KMeta, Kind(..), bindConstraint, substKMeta)
 import Size (Size(..), plusSize, maxSize, sizeConstraintFor)
 import Syntax (Type(..))
 import qualified Syntax
@@ -51,34 +53,28 @@ makeSizeTerm ::
   Map Text Kind ->
   Vector Text ->
   Vector Kind ->
-  Vector SMeta ->
+  Map (Constraint (Either TMeta (Var Int Void))) SMeta ->
   Vector (Type (Var Int Void)) ->
-  m (Size (Either SMeta Void))
-makeSizeTerm kindScope paramNames paramKinds sizeVars =
-  foldlM (\acc a -> plusSize acc <$> typeSizeTerm a) (Word 0)
+  m (Set SMeta, Size (Either SMeta Void))
+makeSizeTerm kindScope paramNames paramKinds assumedConstraints argTypes = do
+  global <- use globalTheory
+  let
+    theory :: Theory (Either TMeta (Var Int Void))
+    theory =
+      Theory
+      { _thGlobal = global
+      , _thLocal = assumedConstraints
+      }
+  foldlM
+    (\(usedSizeMetas, sz) a -> do
+        sz' <- typeSizeTerm theory a
+        pure (usedSizeMetas <> foldMap (either Set.singleton absurd) sz', plusSize sz sz')
+    )
+    (mempty, Word 0)
+    argTypes
   where
-    typeSizeTerm :: Type (Var Int Void) -> m (Size (Either SMeta Void))
-    typeSizeTerm t = do
-      global <- use globalTheory
-      local <-
-        Vector.ifoldM
-          (\acc ix s -> do
-              k <- solveKMetas $ paramKinds Vector.! ix
-              pure $
-                Map.insert
-                (Right . unvar (\() -> B ix) F <$> sizeConstraintFor 0 k)
-                s
-                acc
-          )
-          mempty
-          sizeVars
-      let
-        theory :: Theory (Either TMeta (Var Int Void))
-        theory =
-          Theory
-          { _thGlobal = global
-          , _thLocal = local
-          }
+    typeSizeTerm :: Theory (Either TMeta (Var Int Void)) -> Type (Var Int Void) -> m (Size (Either SMeta Void))
+    typeSizeTerm theory t = do
       placeholder <- freshSMeta
       (_assumes, subs) <-
         solve
@@ -86,8 +82,35 @@ makeSizeTerm kindScope paramNames paramKinds sizeVars =
           (unvar (paramNames Vector.!) absurd)
           (unvar (paramKinds Vector.!) absurd)
           theory
-          [(placeholder, CSized $ fmap Right t)]
+          [(placeholder, CSized $ Right <$> t)]
       pure $ findSMeta subs placeholder
+
+-- | Given some assumptions and an instance head, construct an axiom type
+makeSizeConstraint ::
+  Vector (Constraint (Either TMeta (Var Int Void))) ->
+  Type (Either TMeta (Var Int Void)) ->
+  Constraint Void
+makeSizeConstraint as = _ $ go 0 (Vector.toList as)
+  where
+    go ::
+      Int -> -- the remaining entries
+      [Constraint (Either TMeta (Var Int Void))] ->
+      Type (Either TMeta (Var Int Void)) ->
+      Constraint (Either TMeta (Var Int Void))
+    go !n assumes hd =
+      case assumes of
+        [] -> CSized hd
+        a:rest ->
+          let
+            hd' = go (n+1) rest hd
+          in
+            CForall (error "TODO") (error "TODO") . CImplies (F <$> a) $
+            bindConstraint
+              (either
+                 (TVar . F . Left)
+                 (unvar (\ix -> if ix == n then TVar $ B () else TVar $ F $ Right $ B ix) absurd)
+              )
+              hd'
 
 checkADT ::
   forall s m.
@@ -102,7 +125,7 @@ checkADT ::
   Text -> -- name
   Vector Text -> -- type parameters
   Syntax.Ctors (Var Int Void) -> -- constructors
-  m (Kind, Size Void)
+  m (Kind, Constraint Void, Size Void)
 checkADT kScope datatypeName paramNames ctors = do
   datatypeKind <- KVar <$> freshKMeta
   paramMetas <- Vector.replicateM (Vector.length paramNames) freshKMeta
@@ -119,14 +142,45 @@ checkADT kScope datatypeName paramNames ctors = do
 
   sizeMetas <- Vector.replicateM (Vector.length paramNames) freshSMeta
   paramKinds <- traverse (fmap (substKMeta $ const KType) . solveKMetas . KVar) paramMetas
-  sz <-
+  let
+    assumedConstraintsFwd :: Map (Constraint (Either TMeta (Var Int Void))) SMeta
+    assumedConstraintsBwd :: Map SMeta (Constraint (Either TMeta (Var Int Void)))
+    (assumedConstraintsFwd, assumedConstraintsBwd) =
+      Vector.ifoldl'
+        (\(accFwd, accBwd) ix s ->
+           let
+             k = paramKinds Vector.! ix
+             c = Right . unvar (\() -> B ix) F <$> sizeConstraintFor 0 k
+           in
+             ( Map.insert c s accFwd
+             , Map.insert s c accBwd
+             )
+        )
+        (mempty, mempty)
+        sizeMetas
+
+  (usedSizeMetas, sz) <-
     adtSizeTerm
       kindScope
       paramKinds
-      sizeMetas
+      assumedConstraintsFwd
       0
+      mempty
       (Word 0)
       ctors
+
+  let
+    usedConstraints :: Vector (Constraint (Either TMeta (Var Int Void)))
+    usedConstraints =
+      Vector.map (assumedConstraintsBwd Map.!) $
+      Vector.filter (`Set.member` usedSizeMetas) sizeMetas
+
+    axiomHead :: Type (Either TMeta (Var Int Void))
+    axiomHead =
+      foldl
+        (\acc ix -> TApp acc (TVar $ Right $ B ix))
+        (TName datatypeName)
+        [0..length paramNames - 1]
 
   let
     m_sz' =
@@ -142,6 +196,7 @@ checkADT kScope datatypeName paramNames ctors = do
     Just sz' ->
       pure
         ( IR.substKMeta (const KType) datatypeKind''
+        , makeSizeConstraint usedConstraints axiomHead
         , sz'
         )
   where
@@ -169,17 +224,19 @@ checkADT kScope datatypeName paramNames ctors = do
     adtSizeTerm ::
       Map Text Kind ->
       Vector Kind ->
-      Vector SMeta ->
+      Map (Constraint (Either TMeta (Var Int Void))) SMeta ->
       Int ->
+      Set SMeta ->
       Size (Either SMeta Void) ->
       Syntax.Ctors (Var Int Void) -> -- constructors
-      m (Size (Either SMeta Void))
-    adtSizeTerm kindScope paramKinds sizeMetas !ctorCount sz c =
+      m (Set SMeta, Size (Either SMeta Void))
+    adtSizeTerm kindScope paramKinds assumedConstraints !ctorCount !usedConstraints sz c =
       case c of
         Syntax.End ->
+          pure $!
           if ctorCount > 1
-          then pure $ plusSize (Word 8) sz
-          else pure sz
+          then (usedConstraints, plusSize (Word 8) sz)
+          else (usedConstraints, sz)
         Syntax.Ctor _ctorName ctorArgs ctorRest -> do
           traverse_
             (\ty ->
@@ -191,11 +248,12 @@ checkADT kScope datatypeName paramNames ctors = do
             )
             ctorArgs
 
-          sz' <- makeSizeTerm kindScope paramNames paramKinds sizeMetas ctorArgs
+          (usedConstraints', sz') <- makeSizeTerm kindScope paramNames paramKinds assumedConstraints ctorArgs
           adtSizeTerm
             kindScope
             paramKinds
-            sizeMetas
+            assumedConstraints
             (ctorCount+1)
+            (usedConstraints <> usedConstraints')
             (maxSize sz sz')
             ctorRest
