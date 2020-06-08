e314b8d6d732eaa3c6cf78ffd60ab406e7613563
Isaac Elliott <isaace71295@gmail.com>
2020-06-08 20:21:13 +1000

    more parsing. decimal should return integral

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,9 +1,40 @@
+{-# language OverloadedStrings #-}
 module Parser () where
 
+import Control.Applicative ((<|>), many, optional)
+import Data.Foldable (foldl')
 import Data.Void (Void)
-import Text.Sage (Parser)
+import Text.Sage (Parser, (<?>))
+import qualified Text.Sage as Parser
 
 import Syntax (Expr(..), Type(..))
 
 expr :: Parser s (Expr Void)
-expr = _
+expr = undefined
+  where
+    bool =
+      BTrue <$ Parser.text "true" <?> "true" <|>
+      BFalse <$ Parser.text "false" <?> "false"
+
+    new =
+      New <$ Parser.text "new" <* Parser.char '[' <*>
+      expr <* Parser.char ']'
+
+    project =
+      foldl Project <$> atom <*> many field
+
+    deref =
+      maybe id (\() -> Deref) <$> optional (Parser.char '*') <*>
+      project
+
+    number =
+      (\f -> Number . f) <$>
+      (negate <$ Parser.char '-' <|> pure id) <*>
+      Parser.decimal
+
+    atom =
+      bool <|>
+      new <|>
+      number
+
+    field = _
