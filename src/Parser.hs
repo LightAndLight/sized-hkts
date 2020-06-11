0cfb733995028339ef8ae0f25486871cc68ba2d9
Isaac Elliott <isaace71295@gmail.com>
2020-06-11 18:50:40 +1000

    working on match expression

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,16 +1,20 @@
+{-# language LambdaCase #-}
 {-# language OverloadedStrings #-}
 module Parser (Parser, Parser.ParseError(..), Parser.parse, Parser.eof, expr) where
 
+import Bound (Var(..))
 import Control.Applicative ((<|>), many)
+import Data.Text (Text)
 import qualified Data.Vector as Vector
-import Data.Void (Void)
 import Text.Sage (Parser, (<?>))
 import qualified Text.Sage as Parser
 
-import Syntax (Expr(..))
+import Syntax (Case(..), Expr(..))
 
-expr :: Parser s (Expr Void)
-expr = deref
+expr :: (Text -> Maybe a) -> Parser s (Expr a)
+expr abstract =
+  match <|>
+  deref
   where
     bool =
       BTrue <$ Parser.symbol "true" <|>
@@ -18,17 +22,19 @@ expr = deref
 
     new =
       New <$ Parser.symbol "new" <* Parser.char '[' <*>
-      expr <* Parser.char ']'
+      expr abstract <* Parser.char ']'
 
     field =
       Parser.takeWhile1 Parser.pDigit <|>
       Parser.takeWhile1 Parser.pLower
 
+    commasep p = Parser.sepBy p (Parser.char ',' *> spaces)
+
     args =
       Parser.between
         (Parser.char '(')
         (Parser.char ')')
-        (Vector.fromList <$> Parser.sepBy expr (Parser.char ',' *> spaces))
+        (Vector.fromList <$> commasep (expr abstract))
 
     projectOrCall =
       foldl (\acc -> either (Project acc) (Call acc)) <$>
@@ -44,12 +50,34 @@ expr = deref
       (negate <$ Parser.char '-' <|> pure id) <*>
       Parser.decimal
 
-    spaces = Parser.takeWhile1 ((== ' '), "space")
+    spaces = many $ Parser.satisfy ((== ' '), "space")
 
-    ident = Name <$> Parser.takeWhile1 Parser.pLower <?> "identifier"
+    newlines = many $ Parser.satisfy (\case; '\n' -> True; ' ' -> True; _ -> False, "newline")
+
+    ident = Parser.takeWhile1 Parser.pLower <?> "identifier"
+
+    parens = Parser.between (Parser.char '(') (Parser.char ')')
 
     atom =
       bool <|>
       new <|>
       number <|>
-      ident
+      (\i -> maybe (Name i) Var $ abstract i) <$> ident <|>
+      parens (expr abstract)
+
+    case_ = do
+      c <- ident
+      as <- parens (Vector.fromList <$> commasep ident)
+      _ <- spaces *> Parser.symbol "=>" *> spaces
+      e <- expr (\n -> B <$> Vector.findIndex (n ==) as <|> F <$> abstract n)
+      pure $ Case c as e
+
+    match =
+      Match <$ Parser.symbol "match" <* spaces <*>
+      deref <* spaces <*>
+      Parser.between
+        (Parser.char '{' *> newlines)
+        (Parser.char '}')
+        (Vector.fromList <$>
+         Parser.sepBy case_ (Parser.char ';' <* newlines)
+        )
--- test/Test/Parser.hs
+++ test/Test/Parser.hs
@@ -2,37 +2,48 @@
 {-# language OverloadedLists #-}
 module Test.Parser (parserTests) where
 
+import Bound (Var(..))
+import Data.Void (Void)
 import Test.Hspec
 
-import Syntax (Expr(..))
+import Syntax (Case(..), Expr(..))
 import Parser (parse, eof, expr)
 
 parserTests :: Spec
 parserTests =
   describe "parser" $ do
     it "true" $ do
-      parse (expr <* eof) "true" `shouldBe` Right BTrue
+      parse (expr (const (Nothing :: Maybe Void)) <* eof) "true" `shouldBe` Right BTrue
     it "false" $ do
-      parse (expr <* eof) "false" `shouldBe` Right BFalse
+      parse (expr (const (Nothing :: Maybe Void)) <* eof) "false" `shouldBe` Right BFalse
     it "1234" $ do
-      parse (expr <* eof) "1234" `shouldBe` Right (Number 1234)
+      parse (expr (const (Nothing :: Maybe Void)) <* eof) "1234" `shouldBe` Right (Number 1234)
     it "-1234" $ do
-      parse (expr <* eof) "-12345" `shouldBe` Right (Number (-12345))
+      parse (expr (const (Nothing :: Maybe Void)) <* eof) "-12345" `shouldBe` Right (Number (-12345))
     it "hello" $ do
-      parse (expr <* eof) "hello" `shouldBe` Right (Name "hello")
+      parse (expr (const (Nothing :: Maybe Void)) <* eof) "hello" `shouldBe` Right (Name "hello")
     it "f()" $ do
-      parse (expr <* eof) "f()" `shouldBe` Right (Call (Name "f") [])
+      parse (expr (const (Nothing :: Maybe Void)) <* eof) "f()" `shouldBe` Right (Call (Name "f") [])
     it "f(a, b)" $ do
-      parse (expr <* eof) "f(a, b)" `shouldBe` Right (Call (Name "f") [Name "a", Name "b"])
+      parse (expr (const (Nothing :: Maybe Void)) <* eof) "f(a, b)" `shouldBe` Right (Call (Name "f") [Name "a", Name "b"])
     it "x.y" $ do
-      parse (expr <* eof) "x.y" `shouldBe` Right (Project (Name "x") "y")
+      parse (expr (const (Nothing :: Maybe Void)) <* eof) "x.y" `shouldBe` Right (Project (Name "x") "y")
     it "x.0" $ do
-      parse (expr <* eof) "x.0" `shouldBe` Right (Project (Name "x") "0")
+      parse (expr (const (Nothing :: Maybe Void)) <* eof) "x.0" `shouldBe` Right (Project (Name "x") "0")
     it "*x" $ do
-      parse (expr <* eof) "*x" `shouldBe` Right (Deref (Name "x"))
+      parse (expr (const (Nothing :: Maybe Void)) <* eof) "*x" `shouldBe` Right (Deref (Name "x"))
     it "**x" $ do
-      parse (expr <* eof) "**x" `shouldBe` Right (Deref $ Deref $ Name "x")
+      parse (expr (const (Nothing :: Maybe Void)) <* eof) "**x" `shouldBe` Right (Deref $ Deref $ Name "x")
     it "new[0]" $ do
-      parse (expr <* eof) "new[0]" `shouldBe` Right (New (Number 0))
+      parse (expr (const (Nothing :: Maybe Void)) <* eof) "new[0]" `shouldBe` Right (New (Number 0))
     it "*new[0]" $ do
-      parse (expr <* eof) "*new[0]" `shouldBe` Right (Deref $ New (Number 0))
+      parse (expr (const (Nothing :: Maybe Void)) <* eof) "*new[0]" `shouldBe` Right (Deref $ New (Number 0))
+    it "match x { Left(e) => e(1), Right(a) => 1 }" $ do
+      parse (expr (const (Nothing :: Maybe Void)) <* eof) "match x { Left(e) => e(1), Right(a) => 1 }" `shouldBe`
+        Right
+          (Match
+            (Name "x")
+            [ Case "Left" ["e"] (Call (Var $ B 0) [Number 1])
+            , Case "Right" ["a"] (Number 1)
+            ]
+          )
